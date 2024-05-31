27a8f5320fec932d33f154320ae5900e5434e7bf
LCmaggielam <maggielam@lanecrawford.com>
2024-05-31 11:41:21 +0800

    Update test_adder.py

--- 07_Fixtures_Activity/my_library/tests/test_adder.py
+++ 07_Fixtures_Activity/my_library/tests/test_adder.py
@@ -2,5 +2,17 @@
 import pytest
 from .. import my_module
 
+@pytest.fixture(scope="session")
+def adder_fixture():
+    adder = my_module.SingleObject(5)
+    return adder
+
+def test_add_5(adder_fixture):
+    total = adder_fixture.add_more(5)
+    assert total == 10
+
+def test_add_6(adder_fixture):
+    total = adder_fixture.add_more(6)
+    assert total == 11
 
 
